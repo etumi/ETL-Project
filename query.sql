cc603e1fdc1cbae29c8811230ab58de77c690126
Rahul Sanghera <rahulsanghera@MacBook-Pro.local>
2020-04-29 17:53:05 -0400

    Final Version

--- query.sql
+++ query.sql
@@ -1,15 +1,16 @@
 -- Get the directors name from rotten tomatoes db
 
-SELECT  i.imdb_ID, i.title, i.year, r.director
-FROM public."IMDB" AS i, public."rotten_tomatoes" AS r
-WHERE i.query_title = r.query_title
+SELECT i.imdb_id,i.title, i.year, r.director
+FROM public."IMDB" AS i, public."Rotten_Tomatoes" AS r
+WHERE i.query_title = r.query_title;
 
 -- Get imdb rating vs number of votes
-SELECT i.title, i.votes, o.imdb_ID, o.imdb_rating 
+SELECT i.title, i.votes, o.imdb_id, o.imdb_rating 
 FROM public."IMDB" AS i, public."OMDB" AS o
-WHERE i.imdb_ID = r.imdb_ID
+WHERE i.imdb_id = o.imdb_id
 
 -- Get IMDB and rotten tomatoes score
-SELECT i.title, o.imdb_ID, o.imdb_rating , o.rotten_rating
+
+SELECT i.title, o.imdb_id, o.imdb_rating , o.rotten_rating
 FROM public."IMDB" AS i, public."OMDB" AS o
-WHERE i.imdb_ID = r.imdb_ID
+WHERE i.imdb_id = o.imdb_id
